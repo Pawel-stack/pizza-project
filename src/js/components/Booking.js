134ed217047a4a6cdf4b2abed419d161a7f7aedc
Pawel Krol <paullkroll@icloud.com>
2023-06-04 16:19:10 +0200

    Calendar fix - next step is hourslider

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,3 +1,5 @@
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 import { select, templates } from './../settings.js';
 import utils from './../utils.js';
 import AmountWidget from './AmountWidget.js';
@@ -25,7 +27,8 @@ class Booking{
       wrapper: element,
       peopleAmount: element.querySelector(select.booking.peopleAmount),
       hoursAmount: element.querySelector(select.booking.hoursAmount),
-      datePicker: element.querySelector(select.widgets.datePicker),
+      datePicker: element.querySelector(select.widgets.datePicker.wrapper),
+      hourPicker: element.querySelector(select.widgets.hourPicker.wrapper),
     };
 
   }
@@ -35,11 +38,20 @@ class Booking{
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
     
     thisBooking.dom.peopleAmount.addEventListener('updated', function(){
     });
 
     thisBooking.dom.hoursAmount.addEventListener('updated', function(){
+    });
+
+    thisBooking.dom.datePicker.addEventListener('updated', function(){
+
+    });
+    thisBooking.dom.hourPicker.addEventListener('updated', function(){
+
     });
   }
 }
